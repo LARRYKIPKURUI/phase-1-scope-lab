4c1f2d942f8d7bbf9a88e25b22c19d7b47349ce3
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-03-06 20:53:09 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer = "someone"; 
+
+// Function Definitions
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"; 
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "someone else";
+}
